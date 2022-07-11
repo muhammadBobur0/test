2dfa93215e6f5f77ffac39d13e5432b65a6dbcf5
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-07-11 22:08:07 +0500

    first commit

---
+++ src/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export default () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: "10px" }}>
+    <div className="lds-dual-ring" />
+  </div>
+);
